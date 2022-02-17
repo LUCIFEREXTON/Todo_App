fc45f23e1371500b4ee449f64fa0b04c2c2db638
LUCIFEREXTON <57142378+LUCIFEREXTON@users.noreply.github.com>
2022-02-17 20:20:09 +0530

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,5 @@ class User < ApplicationRecord
   has_secure_password
   validates :password, presence: true
   validates :name, presence: true, uniqueness: true
-  has_many :users, class_name: "user", foreign_key: "reference_id"
+  has_many :lists, class_name: "list", foreign_key: "reference_id"
 end
